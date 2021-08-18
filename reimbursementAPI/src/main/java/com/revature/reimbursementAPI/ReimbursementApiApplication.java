1c1d257c7d9c14d4a32ba59e069f9ee1d6e3850a
AudreyKK <aukkeefe@gmail.com>
2021-08-18 16:18:49 -0400

    blank spring proj

---
+++ reimbursementAPI/src/main/java/com/revature/reimbursementAPI/ReimbursementApiApplication.java
@@ -0,0 +1,13 @@
+package com.revature.reimbursementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimbursementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimbursementApiApplication.class, args);
+	}
+
+}
