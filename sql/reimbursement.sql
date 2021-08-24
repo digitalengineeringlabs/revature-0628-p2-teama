03d24ea1f75b8a242f6519ddbc2b0aabb1b7fd50
Nicholas Card <nicholas.card@revature.com>
2021-08-23 20:18:28 -0400

    final changes to schema

--- sql/reimbursement.sql
+++ sql/reimbursement.sql
@@ -28,12 +28,12 @@ CREATE TABLE reimbursementschema.ticket (
 			references reimbursementschema.employee(employee_id)
 );
 
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('nick','card', 'employee', 'nickcard@email.com', 'nickcard');
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('Anthoney','Newsome', 'employee', 'anewsome@email.com', 'anthoneynewsome');
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('Kha','lee', 'employee', 'klee@email.com', 'khalee');
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('Audrey','Keefe', 'manager', 'akeefe@email.com', 'audreykeefe');
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('test','1', 'manager', 'test1@email.com', 'test1');
-INSERT INTO reimbursementschema.employee (f_name,l_name, role, username, password) VALUES ('test','2', 'employee', 'test2@email.com', 'test2');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('nick','card', 'employee', 'nickcard@email.com', 'nickcard');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('Anthoney','Newsome', 'employee', 'anewsome@email.com', 'anthoneynewsome');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('Kha','lee', 'employee', 'klee@email.com', 'khalee');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('Audrey','Keefe', 'manager', 'akeefe@email.com', 'audreykeefe');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('test','1', 'manager', 'test1@email.com', 'test1');
+INSERT INTO reimbursementschema.employee (first_name,last_name, role, username, password) VALUES ('test','2', 'employee', 'test2@email.com', 'test2');
 
 insert into reimbursementschema.ticket (status, note, type, amount, date_made, date_resolved, employee_id) values ('pending', 'got some mcdonalds', 'food', 10.0, '2021/06/07', '2021/06/08', 5);
 insert into reimbursementschema.ticket (status, note, type, amount, date_made, date_resolved, employee_id) values ('approved', 'gas', 'travel', 50.0, '2021/11/12', null, 5);
