b2c38a4e96cdab760d857a4b79b88038bc80c833
Nicholas Card <nicholas.card@revature.com>
2021-08-25 12:48:34 -0400

    inputs return to blank after submit

--- client/src/app/tickets.ts
+++ client/src/app/tickets.ts
@@ -1,18 +1,9 @@
 export interface Ticket {
-
-    //some of these are handled by the backend and 
-    //some of them will be hard coded
-    //like todays date and hopefully the employeeId
-
-    //need to figure out how to send the employee id without
-    //having it in this ticket ts 
     id: number;
     ticketType: string;
     status: string;
     note: string;
     amount: string;
     employeeId: number;
-    //have to learn how this works after getting it to 
-    //sumbit
-    //dateMade: Date;
+    
 }
