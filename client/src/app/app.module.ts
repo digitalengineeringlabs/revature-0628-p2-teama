89b2e31672ed6098434707f93f8104859cf58f5e
Anewsome09 <anthoney.newsome@revature.net>
2021-08-26 12:28:51 -0400

    Test 1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './Login/login.component';
+import { HomePageComponent } from './Login/homePage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Login/auth.guard';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
+const routes:Routes = [
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard], children: 
+  [{ path: 'employee', component: EmployeeComponent}]},
+  
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    LoginComponent,
+    HomePageComponent
   ],
+
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule, 
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/employee/employee.component.ts
+++ client/src/app/employee/employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
-import { EmployeeService } from '../employee.service';
+import { Employee } from '../../employee';
+import { EmployeeService } from '../../employee.service';
 
 @Component({
   selector: 'app-employee',
